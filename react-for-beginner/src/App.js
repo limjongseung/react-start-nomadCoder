60b462984ab02697235ea6d6ecdebd2a3949323b
limjongseung <101609328+limjongseung@users.noreply.github.com>
2024-02-22 00:37:51 +0900

    cleanup function

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -1,30 +1,22 @@
-import Button from "./Button";
-import styles from "./App.module.css";
 import { useEffect, useState } from "react";
+function Hello() {
+	function byeFn() {
+		console.log("bye :(");
+	}
+	function hiFn() {
+		console.log("created :)");
+		return byeFn;
+	}
+	useEffect(hiFn, []);
+	return <h1>Hello!</h1>;
+}
 function App() {
-	const [counter, setValue] = useState(0);
-	const [keyword, setKeyword] = useState("");
-	const onClick = () => setValue((prev) => prev + 1);
-	const onChange = (event) => setKeyword(event.target.value);
-	console.log("i run all the time");
-	const iRunOnlyOnce = () => {
-		console.log('i run when "counter" changed');
-	};
-	useEffect(iRunOnlyOnce, [counter]);
-	useEffect(() => {
-		console.log('i run when "keyword" changed');
-	}, [keyword]);
+	const [state, setState] = useState(false);
+	const onClick = () => setState((prev) => !prev);
 	return (
 		<div>
-			<input
-				type="text"
-				placeholder="Search here"
-				value={keyword}
-				onChange={onChange}
-			/>
-			<h1 className={styles.title}>welcome</h1>
-			<button onClick={onClick}>click me</button>
-			<div>{counter}</div>
+			{state ? <Hello /> : null}
+			<button onClick={onClick}>{state ? "hide" : "show"}</button>
 		</div>
 	);
 }
