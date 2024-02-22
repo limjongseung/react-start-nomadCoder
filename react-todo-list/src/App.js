27546a8cde90e367b7a9372bc1e4125d9319baac
limjongseung <101609328+limjongseung@users.noreply.github.com>
2024-02-22 14:53:33 +0900

    react-todo-list

--- react-todo-list/src/App.js
+++ react-todo-list/src/App.js
@@ -1,10 +1,32 @@
-
-
-
+import { useState } from "react";
 function App() {
-  return (
-    <div>hi</div>
-  )
+	const [toDo, setToDo] = useState("");
+  const [toDos,setToDos]   = useState([]);
+	const onChange = (e) => setToDo(e.target.value);
+	const onSubmit = (e) => {
+		e.preventDefault();
+    if(toDo ===""){
+      return;
+    }
+    setToDo("");
+    setToDos(currentArray => [toDo,...currentArray])
+	};
+  console.log(toDos);
+	console.log(toDo);
+	return (
+		<div>
+      <h1>My ToDo({toDos.length})</h1>
+			<form onSubmit={onSubmit}>
+				<input
+					value={toDo}
+					onChange={onChange}
+					type="text"
+					placeholder="Write your to do..."
+				/>
+				<button>Add To Do</button>
+			</form>
+		</div>
+	);
 }
 
 export default App;
